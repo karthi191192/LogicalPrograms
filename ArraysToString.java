8801fda6332f7a3183cfed453d4fa3a2c4e52c05
Ajayfitrezo <kajay120124@gmail.com>
2023-09-07 06:05:37 -0700

    (how to convert an array to string)

---
+++ ArraysToString.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class ArrayToString
+{
+    public static void main(String args []){
+        String value[]={a,j,a,y};
+        String ans=Arrays.ArrayToString(value);
+        System.out.println(ans);
+
+    }
+}
