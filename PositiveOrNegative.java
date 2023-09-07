8850f12eb099fe9948783a7f6454bdf28f2ef4ab
stuvels@123 <monishamahadevan9828@gmail.com>
2023-09-07 18:54:52 +0530

    To find the given number is positive or negative

---
+++ PositiveOrNegative.java
@@ -0,0 +1,17 @@
+package LogicalPrograms;
+
+public class PositiveOrNegative
+{
+    public static void main(String[]args)
+    {
+       int a=-10;
+       if(a>=0)
+       {
+            System.out.println("The given number is Positive");
+       } 
+       else
+       {
+         System.out.println("The given number is Negative");
+       }
+    }
+}
