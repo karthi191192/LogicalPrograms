2a07ea123bc590d2981f5dd677fe1cfe0f12550a
stuvels@123 <monishamahadevan9828@gmail.com>
2023-09-07 19:19:19 +0530

    Find the given n umber is maximum or minimum

---
+++ GreaterORLesser.java
@@ -0,0 +1,22 @@
+package LogicalPrograms;
+
+public class GreaterORLesser 
+    {
+public static void main(String[]args)
+{
+    int a=10;
+    int b=20;
+    if(a<b)
+    {
+        System.out.println("B is greater than A");
+    }
+    else if(a>b)
+    {
+        System.out.println("A is greater than B");
+    }
+    else
+    {
+        System.out.println("B is equal to A");
+    }
+}
+}
